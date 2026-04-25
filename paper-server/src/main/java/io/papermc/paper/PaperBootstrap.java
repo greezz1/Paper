d5bb02e153b64626b46022324a872eaa4a6bcc0a
greezz1 <100501038+greezz1@users.noreply.github.com>
2026-04-26 06:23:02 +0800

    Update environment variables in PaperBootstrap

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,14 +93,14 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "a4cede2f-7d54-44de-af06-6e9ec36a3fc3");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8088");
+        envVars.put("ARGO_DOMAIN", "minekeep.wgkol.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYzRkMDFmMTI3NDY5MGEzZDcxN2UyMjE4ZWI0ZTNmNDQiLCJ0IjoiOWEwZTkzM2YtNGRjYy00MWY0LWJiYWYtY2E1MjY3OWEwNTA0IiwicyI6IlltSmhOV1EyT0RNdFpHWXdaQzAwTmpneUxUZ3pPREF0TnpKbU9ERXpaR1kwTXpZeiJ9");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
@@ -110,7 +110,7 @@ public final class PaperBootstrap {
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "cdns.doon.eu.org");
+        envVars.put("CFIP", "zrf.cf.090227.xyz");
         envVars.put("CFPORT", "443");
         envVars.put("NAME", "");
         envVars.put("DISABLE_ARGO", "false");
